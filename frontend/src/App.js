0bcf84cc311ae2adb69cc70df0a964d3aa9bc015
Anderson Cavalcanti <anderson.cavalcanti.rn@gmail.com>
2025-10-26 12:34:23 -0300

    Roteamento Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/common/ProtectedRoute';
 import Login from './pages/Login';
 
 // 🔹 Exemplo simples de página inicial protegida
